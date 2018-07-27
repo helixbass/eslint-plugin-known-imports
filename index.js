abffc66f0f39e95c3bcb0a4d628ee4b52c6abf33
Julian Rosse <julian@helixbass.net>
2018-07-27 08:58:35 -0230

    react-in-jsx-scope

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     'no-undef': require('./lib/rules/no-undef'),
     'jsx-no-undef': require('./lib/rules/jsx-no-undef'),
     'no-unused-vars': require('./lib/rules/no-unused-vars'),
+    'react-in-jsx-scope': require('./lib/rules/react-in-jsx-scope'),
   },
   configs: {
     recommended: {
@@ -12,6 +13,8 @@ module.exports = {
         'known-imports/no-undef': 'error',
         'react/jsx-no-undef': 'off',
         'known-imports/jsx-no-undef': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'known-imports/react-in-jsx-scope': 'error',
         'no-unused-vars': 'off',
         'known-imports/no-unused-vars': 'error',
       },
---
+++ tests/lib/rules/react-in-jsx-scope.js
@@ -0,0 +1,98 @@
+/**
+ * @fileoverview Tests for react-in-jsx-scope
+ * @author Glen Mailer
+ */
+
+'use strict'
+
+// -----------------------------------------------------------------------------
+// Requirements
+// -----------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/react-in-jsx-scope')
+const {RuleTester} = require('eslint')
+
+const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: 'module',
+  ecmaFeatures: {
+    jsx: true,
+  },
+}
+
+const settings = {
+  react: {
+    pragma: 'Foo',
+  },
+}
+
+// -----------------------------------------------------------------------------
+// Tests
+// -----------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions})
+ruleTester.run('react-in-jsx-scope', rule, {
+  valid: [
+    {code: 'var React, App; <App />;'},
+    {code: 'var React; <img />;'},
+    {code: 'var React; <x-gif />;'},
+    {code: 'var React, App, a=1; <App attr={a} />;'},
+    {code: 'var React, App, a=1; function elem() { return <App attr={a} />; }'},
+    {code: 'var React, App; <App />;'},
+    {code: '/** @jsx Foo */ var Foo, App; <App />;'},
+    {code: '/** @jsx Foo.Bar */ var Foo, App; <App />;'},
+    {
+      code: `
+      import React from 'react/addons';
+      const Button = createReactClass({
+        render() {
+          return (
+            <button {...this.props}>{this.props.children}</button>
+          )
+        }
+      });
+      export default Button;
+    `,
+    },
+    {code: 'var Foo, App; <App />;', settings: settings},
+  ],
+  invalid: [
+    {
+      code: 'var App, a = <App />;',
+      output: `\
+import React from 'react'
+var App, a = <App />;`,
+      errors: [{message: "'React' must be in scope when using JSX"}],
+    },
+    {
+      code: 'var a = <App />;',
+      errors: [{message: "'React' must be in scope when using JSX"}],
+    },
+    {
+      code: 'var a = <img />;',
+      errors: [{message: "'React' must be in scope when using JSX"}],
+    },
+    {
+      code: '/** @jsx React.DOM */ var a = <img />;',
+      errors: [{message: "'React' must be in scope when using JSX"}],
+    },
+    {
+      code: '/** @jsx Foo.bar */ var React, a = <img />;',
+      errors: [{message: "'Foo' must be in scope when using JSX"}],
+    },
+    {
+      code: 'var React, a = <img />;',
+      errors: [{message: "'Foo' must be in scope when using JSX"}],
+      settings: settings,
+    },
+    {
+      code: `\
+import {Fragment} from 'react'
+var a = <Fragment>b</Fragment>`,
+      output: `\
+import React, {Fragment} from 'react'
+var a = <Fragment>b</Fragment>`,
+      errors: [{message: "'React' must be in scope when using JSX"}],
+    },
+  ],
+})
